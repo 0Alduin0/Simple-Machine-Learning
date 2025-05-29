acf8895b5821c0f9d5ebbd17c1397636a2014525
Enes Yürekli <muhammedenesyurekli@gmail.com>
2025-05-30 02:15:52 +0300

    bazı kod düzeltmeleri yapıldı

--- main.py
+++ main.py
@@ -3,40 +3,35 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import random
 
+# Tüm bir iterasyon yaptıktan sonra iterasyonları karşılaktırmak için kullanılıyor
+sonuçlar = []
 
-#Tüm bir iterasyon yaptıktan sonra iterasyonları karşılaktırmak için kullanılıyor
-sonuçlar=[]
-
-
-#11 kere 2000 lik veri setinden sonuç çıkaracak
-while(True):
-    #Veriler oluşturuluyor
+# 11 kere 2000 lik veri setinden sonuç çıkaracak
+for _ in range(10):
+    # Veriler oluşturuluyor
     data = {
         "vize": [],
         "final": [],
         "gecti_mi": []
     }
 
-    for _ in range(2000):  #2000 adet veri verilecek
+    for _ in range(10000):  # 2000 adet veri verilecek
         vize_notu = random.randint(0, 100)
         final_notu = random.randint(0, 100)
         data["vize"].append(vize_notu)
         data["final"].append(final_notu)
 
-        if vize_notu*0.4 + final_notu*0.6 >= 50:
+        if (vize_notu * 0.4 + final_notu * 0.6 >= 50):
             data["gecti_mi"].append(1)
         else:
             data["gecti_mi"].append(0)
 
-
-
-    #veri tabloya dönüştürülüyor
+    # veri tabloya dönüştürülüyor
     df = pd.DataFrame(data)
 
     # X (sınav notları) ve y (sınav notlarına göre geçme kalma durumu)
     X = df[["vize", "final"]]  # Girişler
-    y = df["gecti_mi"]         # Etiket
-
+    y = df["gecti_mi"]  # Etiket
 
     # Verileri eğitim ve test olacak şekilde bölüyoruz.
     # Train ile bitenlerle öğrenme test ile bitenlerle alıştırma yapıyor.
@@ -58,12 +53,12 @@ while(True):
     #print("Tahmin:", tahminler.tolist())
 
     # kendi verdiğmiz veriyle algoritmayı test ediyoruz
-    girilen_vize=input("Vize Notu: ")
-    girilen_final=input("Final Notu: ")
-    yeni_ogrenci = pd.DataFrame([[girilen_vize, girilen_final]], columns=["vize", "final"])
+    #girilen_vize = input("Vize Notu: ")
+    #girilen_final = input("Final Notu: ")
+    #yeni_ogrenci = pd.DataFrame([[girilen_vize, girilen_final]], columns=["vize", "final"])
+
+    yeni_ogrenci = pd.DataFrame([[50, 82]], columns=["vize", "final"])
     sonuc = model.predict(yeni_ogrenci)
     print("Öğrenci geçti mi?", "Evet\n" if sonuc[0] == 1 else "Hayır\n")
     sonuçlar.append(sonuc)
-
-
 
