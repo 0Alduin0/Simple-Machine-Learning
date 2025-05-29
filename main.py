c9108c9329986b61e4b24959fc769afb6612747a
Enes Yürekli <muhammedenesyurekli@gmail.com>
2025-05-30 01:46:16 +0300

    proje baştan düzeltildi. yorum satırları eklendi. gereksiz kodlar silindi

--- main.py
+++ main.py
@@ -2,49 +2,68 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import random
-# 1. Veri oluştur
-data = {
-    "vize": [],
-    "final": [],
-    "gecti_mi": []
-}
-
-for _ in range(200):  # 200 satır veri için
-    vize_notu = random.randint(0, 100)
-    final_notu = random.randint(0, 100)
-    data["vize"].append(vize_notu)
-    data["final"].append(final_notu)
-
-    if vize_notu*0.4 + final_notu*0.6 >= 50:
-        data["gecti_mi"].append(1)
-    else:
-        data["gecti_mi"].append(0)
 
 
+#Tüm bir iterasyon yaptıktan sonra iterasyonları karşılaktırmak için kullanılıyor
+sonuçlar=[]
 
 
-df = pd.DataFrame(data)
+#11 kere 2000 lik veri setinden sonuç çıkaracak
+while(True):
+    #Veriler oluşturuluyor
+    data = {
+        "vize": [],
+        "final": [],
+        "gecti_mi": []
+    }
 
-# 2. X (giriş) ve y (çıktı) ayrımı
-X = df[["vize", "final"]]  # Girişler
-y = df["gecti_mi"]         # Etiket
+    for _ in range(2000):  #2000 adet veri verilecek
+        vize_notu = random.randint(0, 100)
+        final_notu = random.randint(0, 100)
+        data["vize"].append(vize_notu)
+        data["final"].append(final_notu)
 
-# 3. Veriyi eğitim ve test olarak böl
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+        if vize_notu*0.4 + final_notu*0.6 >= 50:
+            data["gecti_mi"].append(1)
+        else:
+            data["gecti_mi"].append(0)
 
-# 4. Modeli tanımla ve eğit
-model = LogisticRegression()
-model.fit(X_train, y_train)
 
-# 5. Test verisi ile tahmin yap
-tahminler = model.predict(X_test)
 
-# 6. Sonuçları yazdır
-print("Gerçek:", y_test.tolist())
-print("Tahmin:", tahminler.tolist())
+    #veri tabloya dönüştürülüyor
+    df = pd.DataFrame(data)
 
-# 7. Yeni veriyle tahmin (örnek)
-yeni_ogrenci = pd.DataFrame([[49, 50]], columns=["vize", "final"])
-sonuc = model.predict(yeni_ogrenci)
-print("Yeni öğrenci geçti mi?", "Evet" if sonuc[0] == 1 else "Hayır")
+    # X (sınav notları) ve y (sınav notlarına göre geçme kalma durumu)
+    X = df[["vize", "final"]]  # Girişler
+    y = df["gecti_mi"]         # Etiket
+
+
+    # Verileri eğitim ve test olacak şekilde bölüyoruz.
+    # Train ile bitenlerle öğrenme test ile bitenlerle alıştırma yapıyor.
+    # Test_size ise verilen 2000 adet verinin yüzde kaçıyla test yapcağımızı söylüyor.
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    # LogisticRegression sınıflandımra için kullanılan bir algoritmadır.
+    # Bu proje gibi geçme kalma durumlarında iyi çalışır.
+    model = LogisticRegression()
+
+    # modele veriler giriliyor
+    model.fit(X_train, y_train)
+
+    # Test verisi ile tahmin yapılıyor
+    tahminler = model.predict(X_test)
+
+    # Sonuçları yazdırıyoruz 0 kalma 1 geçme demek kullanmak için baştaki işareti silin
+    #print("Gerçek:", y_test.tolist())
+    #print("Tahmin:", tahminler.tolist())
+
+    # kendi verdiğmiz veriyle algoritmayı test ediyoruz
+    girilen_vize=input("Vize Notu: ")
+    girilen_final=input("Final Notu: ")
+    yeni_ogrenci = pd.DataFrame([[girilen_vize, girilen_final]], columns=["vize", "final"])
+    sonuc = model.predict(yeni_ogrenci)
+    print("Öğrenci geçti mi?", "Evet\n" if sonuc[0] == 1 else "Hayır\n")
+    sonuçlar.append(sonuc)
+
+
 
